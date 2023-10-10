d88427e389b86d83e3d73a917f45c9bd395aecca
stanislav-revva <nivadrive1@gmail.com>
2023-10-10 10:10:28 +0300

    commit

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 import MongoConnection from "./MongoConnection.mjs";
 import express  from "express";
+import workerpool from 'workerpool';
+const pool = workerpool.pool('./totalThread.mjs'); //link to file containing thread registration im pool 
 const app = express();
 const port = process.env.port || 8181;
 const server = app.listen(port);
 server.on('listening', () => console.log(`server is listening on port ${port}; processId: ${process.pid}`));
 const dbConnection = new MongoConnection(`mongodb+srv://root:${process.env.MONGO_PASSWORD}@cluster0.axdldgw.mongodb.net/college?retryWrites=true&w=majority`, 'college');
 const studentsCollection = dbConnection.getCollection("students");
+
 app.get('/performance/total', (req,res) => {
-    const startTime = new Date();
     const count = +req.query.count;
-    let total = 0;
-    for (let i = 0; i< count ; i++) {
-        total++;
-    };
-    res.send({pid: process.pid, api: 'node', total, time: new Date().getTime() - startTime.getTime()})
+    pool.exec("total", [count], {
+        on: payload => {
+            if(payload.event == 'partition' ) {
+                res.write(payload.data + '\n');
+                console.log(payload.data);
+            } else {
+
+                res.end(JSON.stringify(payload.data));
+            }     
+        }
+    })
+    
 });
 app.get('/performance/students', async (req, res) => {
     const startTime = new Date();
